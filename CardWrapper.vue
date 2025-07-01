caae9ac9f3d10bc5ec7eaa050fb33733ee46e412
elakkiyasaravanan <elakkiyasaravanan31@gmail.com>
2025-07-02 02:12:16 +0530

    Add files via upload

---
+++ CardWrapper.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="card">
+    <h3>Card Header</h3>
+    <slot name="left"></slot> <!-- First child goes here -->
+    <slot name="right"></slot> <!-- Second child goes here -->
+    <p>Card Footer</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CardWrapper'
+};
+</script>
+
+<style scoped>
+.card {
+  border: 2px solid #ccc;
+  padding: 10px;
+  margin-bottom: 20px;
+}
+</style>
